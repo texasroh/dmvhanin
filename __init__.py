dd691ed1bf1ef3d1e66fd2b302d65e792306af95
June <texasroh@gmail.com>
2020-07-31 01:04:05 -0400

    On Construction

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
@@ -6,7 +6,7 @@ def create_app():
     
     @app.route("/")
     def index():
-        return "Welcome to DMV 한인"
+        return render_template('index.html')
         
     return app
     
