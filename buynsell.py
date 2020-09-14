e2beb33a668f11905a532475156bb3933f63553a
June <texasroh@gmail.com>
2020-09-14 02:54:07 -0400

    modify board content and make history

--- buynsell.py
+++ buynsell.py
@@ -51,12 +51,85 @@ def write():
                 "INSERT INTO {}_{} (user_id, title, contents, ip_address) VALUES (%s, %s, %s, %s)".format(category, board),
                 [g.user['user_id'], title, content, get_client_ip()]
             )
+            board_id = db.select_row(
+               "SELECT LASTVAL()"
+            )['lastval']
             flash('작성 완료')
-            return redirect(url_for('{}.content_list'.format(category), board_name = board))
+            return redirect(url_for('{}.content'.format(category), board_name = board, board_id = board_id))
         
     return render_template('board/write.html', board_list = board_list.to_dict('records'), title=title, content=content)
     
     
+@bp.route('/modify/<board_name>/<int:board_id>', methods=('GET','POST'))
+@login_required
+def modify(board_name, board_id):
+    board_list = get_board_list()
+    if board_name not in list(board_list['board_name']):
+        return redirect(url_for('index'))
+    content = db.select_row(
+        "SELECT * FROM {}_{} WHERE active_flag = TRUE AND board_id = %s".format(category,board_name), [board_id]
+    )
+    if not content:
+        return redirect(url_for('index'))
+    elif g.user['user_id'] != content['user_id']:
+        print(g.user['user_id'], content['user_id'])
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        title = request.form['title']
+        content_textarea = request.form['content']
+        content_textarea = summernote_save_img_to_s3(content_textarea)
+        
+        if not content_textarea:
+            flash('잘못된 이미지파일입니다')
+        elif not title:
+            flash('제목을 입력하세요')
+        else:
+            #히스토리 남기기
+            db.update_rows(
+                "INSERT INTO {}_{} (user_id, title, contents, ip_address, image_url, active_flag, notice_flag, views, parent_id) "\
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) "
+                .format(category, board_name),
+                [content['user_id'], content['title'], content['contents'], content['ip_address'], content['image_url'], False, content['notice_flag'], content['views'], board_id]
+            )
+            bs = BeautifulSoup(content_textarea, 'html.parser')
+            content_textarea = str(bs).replace('<script','').replace('</script','').replace('<iframe','').replace('</iframe','')
+            db.update_rows(
+                "UPDATE {}_{} SET title=%s, contents=%s, ip_address=%s WHERE board_id = %s".format(category, board_name),
+                [title, content_textarea, get_client_ip(), board_id]
+            )
+            flash('수정 완료')
+            return redirect(url_for('{}.content'.format(category), board_name = board_name, board_id = board_id))
+            
+            
+    
+        
+    board_alias = board_list.loc[board_list['board_name'] == board_name, 'board_alias'][0]
+    return render_template('board/write.html', board_list = board_list.to_dict('records'),
+                            title=content['title'], content=content['contents'], board_alias = board_alias, modify = True)
+    
+ 
+@bp.route('/delete/<board_name>/<int:board_id>', methods=('GET',))
+@login_required
+def delete(board_name, board_id):
+    board_list = get_board_list()
+    if board_name not in list(board_list['board_name']):
+        return redirect(url_for('index'))
+    content = db.select_row(
+        "SELECT * FROM {}_{} WHERE active_flag = TRUE AND board_id = %s".format(category,board_name), [board_id]
+    )
+    if not content:
+        return redirect(url_for('index'))
+    elif g.user['user_id'] != content['user_id']:
+        print(g.user['user_id'], content['user_id'])
+        return redirect(url_for('index'))
+
+    db.update_rows(
+        "UPDATE {}_{} SET active_flag=FALSE WHERE board_id = %s".format(category, board_name), [board_id]
+    )
+    return redirect(url_for('{}.content_list'.format(category), board_name=board_name))
+
+
 @bp.route('/<board_name>', methods=('GET', ))
 def content_list(board_name):
     ## url injection check
@@ -67,6 +140,7 @@ def content_list(board_name):
         "SELECT a.*, count(b.*) FROM {0}_{1} a "\
         "LEFT JOIN {0}_{1}_review b "\
         "ON a.board_id = b.board_id "\
+        "WHERE a.active_flag=TRUE "\
         "GROUP BY a.board_id "\
         "ORDER BY 1 DESC".format(category, board_name)
     )
@@ -85,8 +159,6 @@ def content(board_name, board_id):
     board_list = get_board_list()
     if board_name not in list(board_list['board_name']):
         return redirect(url_for('index'))
-    elif not db.select_row("SELECT * FROM {}_{} WHERE board_id = %s".format(category,board_name), [board_id]): 
-        return redirect(url_for('{}.content_list'.format(category), board_name=board_name))
         
     if request.method == "POST":
         ## review 입력하는 로직
@@ -132,10 +204,10 @@ def content(board_name, board_id):
         
     
     content = db.select_row(
-        "SELECT * FROM {}_{} WHERE board_id = %s".format(category, board_name), [board_id]
+        "SELECT * FROM {}_{} WHERE active_flag = TRUE AND board_id = %s".format(category, board_name), [board_id]
     )
     if not content:
-        return redirect(url_for('buynsell.board_list', board_name=board_name))
+        return redirect(url_for('{}.content_list'.format(category), board_name=board_name))
     content['created'] = content['created'].tz_convert('US/Eastern').strftime("%Y-%m-%d (%H:%M)")
     reviews = db.select_rows(
         "SELECT * FROM {}_{}_review WHERE board_id = %s ORDER BY sort".format(category, board_name), [board_id]
@@ -146,5 +218,6 @@ def content(board_name, board_id):
     else:
         reviews['created'] = reviews['created'].dt.tz_convert('US/Eastern').apply(lambda x: x.strftime("%Y-%m-%d (%H:%M)"))
         reviews = reviews.to_dict('records')
-    return render_template('board/content.html', board_list = board_list.to_dict('records'), board_name=board_name, content = content, reviews=reviews)
+    return render_template('board/content.html', board_list = board_list.to_dict('records'),
+                            category=category, board_name=board_name, content = content, reviews=reviews)
     
